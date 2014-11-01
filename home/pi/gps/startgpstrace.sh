5878ce36694b4e4a4136c95eab34dfc6d85aa874
Andreas Kruthoff <kruthoff@gmail.com>
2014-11-01 14:47:40 +0100

    settings for 2.8" and 3.5" TFT.

--- home/pi/gps/startgpstrace.sh
+++ home/pi/gps/startgpstrace.sh
@@ -7,4 +7,8 @@
 # An alternative is to launch gpstrace via /etc/init.d/gpstrace
 
 
-/usr/bin/lxterminal --geometry=60x19 --command /home/pi/gps/gpstrace
+# 2.8" display with terminal monospace 7 font
+#/usr/bin/lxterminal --geometry=60x19 --command /home/pi/gps/gpstrace
+# 3.5" display with terminal monospace 8 font
+/usr/bin/lxterminal --geometry=66x19 --command /home/pi/gps/gpstrace
+
